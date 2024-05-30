169282854d421dfdba24f1ce37a18c2b84a77ed5
GB14KR <130809698+GB14KR@users.noreply.github.com>
2024-05-30 11:51:07 +0900

    Update perceptor.py

--- diffusion_models/perceptor.py
+++ diffusion_models/perceptor.py
@@ -46,7 +46,9 @@ clip_cache = {}
 def get_clip(model_name):
     if model_name not in clip_cache:
         image_fn, text_fn, clip_params, preprocess = clip_jax.load(model_name)
-        clip_params = LazyParams.pt(url=model_name)  # Use the pt method to create LazyParams instance
+        model_path = fetch_model(model_name, '/content/drive/MyDrive/AI/models/')
+        clip_params = LazyParams.pt(model_path)  # Use the pt method to create LazyParams instance
         clip_cache[model_name] = lambda: Perceptor(image_fn, text_fn, clip_params(), preprocess)
     return clip_cache[model_name]()
+
 
